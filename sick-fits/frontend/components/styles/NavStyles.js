326c768b63476d8f25717ec22d30094331ef46d3
pduren <57735971+kParkz@users.noreply.github.com>
2020-06-25 14:46:20 -0500

    nav styles added

--- sick-fits/frontend/components/styles/NavStyles.js
+++ sick-fits/frontend/components/styles/NavStyles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const NavStyles = styled.ul`
   margin: 0;
@@ -13,7 +13,7 @@ const NavStyles = styled.ul`
     align-items: center;
     position: relative;
     text-transform: uppercase;
-    font-weight: 900;
+    font-weight: 500;
     font-size: 1em;
     background: none;
     border: 0;
@@ -23,9 +23,9 @@ const NavStyles = styled.ul`
       padding: 0 10px;
     }
     &:before {
-      content: '';
+      content: "";
       width: 2px;
-      background: ${props => props.theme.lightgrey};
+      background: ${(props) => props.theme.lightgrey};
       height: 100%;
       left: 0;
       position: absolute;
@@ -36,7 +36,7 @@ const NavStyles = styled.ul`
     &:after {
       height: 2px;
       background: red;
-      content: '';
+      content: "";
       width: 0;
       position: absolute;
       transform: translateX(-50%);
@@ -51,13 +51,13 @@ const NavStyles = styled.ul`
       &:after {
         width: calc(100% - 60px);
       }
-    @media (max-width: 700px) {
+      @media (max-width: 700px) {
         width: calc(100% - 10px);
-    }
+      }
     }
   }
   @media (max-width: 1300px) {
-    border-top: 1px solid ${props => props.theme.lightgrey};
+    border-top: 1px solid ${(props) => props.theme.lightgrey};
     width: 100%;
     justify-content: center;
     font-size: 1.5rem;
